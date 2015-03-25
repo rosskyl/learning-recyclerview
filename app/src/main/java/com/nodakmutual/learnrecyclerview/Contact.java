25bb757e0b12971a5af8771c543722d4323424bc
sstutsman <samuel.stutsman@mnstate.edu>
2015-03-25 00:26:46 -0500

    Let's learn RecyclerView

---
+++ app/src/main/java/com/nodakmutual/learnrecyclerview/Contact.java
@@ -0,0 +1,22 @@
+package com.nodakmutual.learnrecyclerview;
+
+/**
+ * Sam Stutsman on March, 2015.
+ */
+public class Contact {
+    private String name;
+    private String address;
+
+    public Contact(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+}
